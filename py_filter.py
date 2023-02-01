df92a7125570adcadfab5d43b1c82cb93be7c037
Luke Tyler <luketyler17@gmail.com>
2023-02-01 09:56:47 -0500

    refactored -- files now output into 2 distinct files, not_found and found.json files

--- py_filter.py
+++ py_filter.py
@@ -9,17 +9,26 @@ argParser = argparse.ArgumentParser()
 argParser.add_argument("-i", "--ifile", help="JSON file to be analyzed", required=True)
 argParser.add_argument("-y", "--yaml", help="YAML Config file that contains keywords to be searched", required=True)
 argParser.add_argument("-d", "--directory", help="Directory to place JSON files in, by default will use current working directory, will create directory if directory passed does not exist")
+argParser.add_argument("-clean", "--clean", action='store', nargs='*', help="Cleans directory of previous files created by this program")
 
 def main(args) -> None:
     if not args.directory:
         #make current working directory the output directory if no directory was provided
         directory = os.getcwd()
         args.directory = os.path.join(directory, "py_filter_output")
-        print(f"\nOutputting all found matches into: {args.directory}\n")
+        print(f"\nOutputting all found matches into: {args.directory}")
     if not os.path.exists(args.directory):
         #creates directory if it doesn't exist
         os.makedirs(args.directory)
-    
+
+    output_found_file = os.path.join(args.directory, "py_filter_found.json")
+    output_not_found_file = os.path.join(args.directory, "not_found.json")
+
+    if args.clean is not None:
+        #clean was called, remove the files this script creates within the directory
+        os.remove(output_found_file)
+        os.remove(output_not_found_file)
+        print(f"\nCleaning py_filter files found in {args.directory}")
     with open(args.yaml) as f:
         #open yaml file for filters
         yaml_data = yaml.load(f, Loader=SafeLoader)
@@ -28,15 +37,62 @@ def main(args) -> None:
         #open json file and iterate through all json objects
         data = json.load(input_file)
         i = 0
+
+        found_dictionary = []
+        unfound_dictionary = []
         while(i < len(data)):
             x = get_path(data[i], yaml_data['filters'])
             if x:
                 #value was found in dictionary, path and value are held within x, i is the index of the dict w/found values
-                fileName = os.path.join(args.directory, x[1]+".json")
-                with open(fileName, "a+") as outFile:
-                    #appends file if exists inside of target directory
-                    json.dump([{x[0] : x[1]}, data[i]], outFile, indent=6)
+                found_dictionary.append(data[i])
+            else:
+                #no value was found in dict, write data to unfound_file
+                unfound_dictionary.append(data[i])
             i += 1
+
+        # If overwriting the same file is fine, use this block
+        # with open(output_found_file, "w+") as outFile:
+        #     #appends file if exists inside of target directory
+        #     json.dump(found_dictionary, outFile, indent=6)
+        #     outFile.close()
+        # with open(output_not_found_file, "w+") as outFile:
+        #     json.dump(unfound_dictionary, outFile, indent=6)
+        #     outFile.close()
+        # input_file.close()
+
+        # if needed to append instead of overwriting, have to pull the JSON out, append to the dictionary then add it back to the file
+        try:
+            #have to load the original file in order to write valid JSON
+            with open(output_found_file) as origin:
+                data = json.load(origin)
+                #adding arrays of dicts together for valid JSON output
+                data = data + found_dictionary
+                origin.close()
+        #if the file does not exist, its not an err, just first time ran/files were cleared
+        except FileNotFoundError as err:
+            data = found_dictionary
+        with open(output_found_file, "w+") as outFile:
+            print("Writing Found Matches")
+            json.dump(data, outFile, indent=6)
+            data = None
+            outFile.close()
+        try:
+            #have to load the original file in order to write valid JSON
+            with open(output_not_found_file) as origin:
+                data = json.load(origin)
+                #adding arrays of dicts together for valid JSON output
+                data = data + unfound_dictionary
+                origin.close()
+        #if the file does not exist, its not an err, just first time ran/files were cleared
+        except FileNotFoundError as err:
+            data = unfound_dictionary
+        with open(output_not_found_file, "w+") as outFile:
+            print("writing unmatched data")
+            json.dump(data, outFile, indent=6)
+            data = None
+            outFile.close()
+        
+        print(f"py_filter finished, found files are in {args.directory}\n{output_found_file} contains json of matches\n{output_not_found_file} contains json with no values found\n")
 
 
 
